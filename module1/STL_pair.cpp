ddee97ccff0e9ed03f629e9e18f9bcdb6691ffeb
Abtahi <163572318+abtahi2024@users.noreply.github.com>
2024-09-27 21:52:23 +0600

    Add files via upload

---
+++ module1/STL_pair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<string,int>p;
+    p=make_pair("rahat",30);
+    // p={"rahat",20};
+    cout<<p.first<<" "<<p.second<<endl;
+    // cout<<p.first<<" "<<p.second<<endl;
+
+    return 0;
+}
